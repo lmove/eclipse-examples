107b9401b960b2ca107df01db7ffa0835e872e3f
Lina Ochoa <lina.m8a@gmail.com>
2017-11-21 13:33:39 +0100

    Adding OSGi standalone configuration.

---
+++ osgi.standalone.configuration/osgi.sh
@@ -0,0 +1,4 @@
+# !/bin/sh
+# For more documentation: https://www.eclipse.org/equinox/documents/quickstart-framework.php
+java -jar org.eclipse.osgi_3.12.50.v20170928-1321.jar -clean -console
+
---
+++ osgi.standalone.configuration/plugins/README.txt
@@ -0,0 +1,1 @@
+Place your framework plugins in this folder. They will be automatically included.
