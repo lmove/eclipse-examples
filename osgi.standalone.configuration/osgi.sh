b0dbaabbcf211cb3d3f83f7c9ef08b07a279c747
Lina Ochoa <lina.m8a@gmail.com>
2018-01-22 14:24:30 +0100

    Updating OSGi standalone configuration + trackers.

--- osgi.standalone.configuration/osgi.sh
+++ osgi.standalone.configuration/osgi.sh
@@ -1,4 +1,4 @@
 # !/bin/sh
 # For more documentation: https://www.eclipse.org/equinox/documents/quickstart-framework.php
-java -jar org.eclipse.osgi_3.12.50.v20170928-1321.jar -clean -console
+java -jar org.eclipse.osgi_3.11.3.v20170209-1843.jar -clean -console
 
